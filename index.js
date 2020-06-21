abc836cbf9f029b6fe67a708b6ebc469d94e7e68
unknown <darkrain.yt.bow@gmail.com>
2020-06-21 22:56:44 +0200

    basic discord bot setup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.once('ready', ready => {
+    console.log("The bot is up and running!");
+    
+});
+
+bot.login("yourToken");
