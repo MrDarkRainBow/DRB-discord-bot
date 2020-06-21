9629dee00e9ef0a37fbc9f4307dc5f255a113601
unknown <darkrain.yt.bow@gmail.com>
2020-06-21 23:03:26 +0200

    added config file and template

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const config = require("./config.json");
 
 bot.once('ready', ready => {
     console.log("The bot is up and running!");
     
 });
 
-bot.login("yourToken");
+bot.login(config.token);
