5ee675bcf09abd3124e207f63adc0749e6b8e953
unknown <darkrain.yt.bow@gmail.com>
2020-06-21 23:33:49 +0200

    added command handler and ping command

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 const config = require("./config.json");
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    bot.commands.set(command.name, command);
+};
 
 bot.once('ready', ready => {
     console.log("The bot is up and running!");
     
+});
+
+bot.on("message", message => {
+
+    //event handler
+    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = bot.commands.get(commandName)
+        || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if(!command) return;
+
+    try{
+        command.execute(message, args);
+    }catch(error){
+        console.error(error);
+        message.reply("Well that didn't work :^|");
+    }
 });
 
 bot.login(config.token);
